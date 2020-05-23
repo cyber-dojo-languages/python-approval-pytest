f8e12156adb548bdb670dbea6acd94be93a22327
Jon Jagger <jon@jaggersoft.com>
2020-05-23 16:43:03 +0100

    Add coverage+style reports to cyber-dojo.sh

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -24,4 +24,31 @@ function delete_dirs()
 # https://github.com/approvals/ApprovalTests.Python
 # https://approvaltests.com/
 
-pytest --approvaltests-use-reporter='PythonNative'
+REPORT_DIR=${CYBER_DOJO_SANDBOX}/report
+rm -rf ${REPORT_DIR} || true
+mkdir -p ${REPORT_DIR}
+
+coverage3 run \
+  --source=${CYBER_DOJO_SANDBOX} \
+  --module pytest \
+    --approvaltests-use-reporter='PythonNative'
+
+# https://coverage.readthedocs.io/en/v4.5.x/index.html
+
+coverage3 report \
+  --show-missing \
+    > ${REPORT_DIR}/coverage.txt
+
+# http://pycodestyle.pycqa.org/en/latest/intro.html#configuration
+
+pycodestyle \
+  ${CYBER_DOJO_SANDBOX} \
+    --show-source `# show source code for each error` \
+    --show-pep8   `# show relevent text from pep8` \
+    --ignore E302,E305,W293 \
+    --max-line-length=80 \
+      > ${REPORT_DIR}/style.txt
+
+# E302 expected 2 blank lines, found 0
+# E305 expected 2 blank lines after end of function or class
+# W293 blank line contains whitespace
