c0cd71a6e58b8e300003cb874a914dba2db49d39
JonJagger <jon@kosli.com>
2026-03-31 08:24:24 +0100

    Simplify version check

--- check_version.sh
+++ check_version.sh
@@ -7,26 +7,12 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly APPROVALTESTS_EXPECTED="approvaltests-17.4.0"
 readonly PYTEST_APPROVALS_EXPECTED="pytest-approvaltests-0.2.4"
-# 'pytest --version --version' returns the plugins used by pytest - we can then use this
-# to find the current versions of approvaltests and pytest-approvaltests
 readonly PLUGINS=$(docker run --rm -i ${IMAGE_NAME} sh -c 'pytest --version --version' | grep 'approvaltests')
 
-APPROVALTESTS_REGEX="approvaltests-[0-9\.]*"
 PYTEST_APPROVALS_REGEX="pytest-approvaltests-[0-9\.]*"
-[[ ${PLUGINS} =~ ${APPROVALTESTS_REGEX} ]]
-readonly APPROVALTESTS_ACTUAL="${BASH_REMATCH[*]}"
 [[ ${PLUGINS} =~ ${PYTEST_APPROVALS_REGEX} ]]
 readonly PYTEST_APPROVALS_ACTUAL="${BASH_REMATCH[*]}"
-
-if echo "${APPROVALTESTS_ACTUAL}" | grep -q "${APPROVALTESTS_EXPECTED}"; then
-  echo "VERSION CONFIRMED as ${APPROVALTESTS_EXPECTED}"
-else
-  echo "VERSION EXPECTED: ${APPROVALTESTS_EXPECTED}"
-  echo "VERSION   ACTUAL: ${APPROVALTESTS_ACTUAL}"
-  exit 42
-fi
 
 if echo "${PYTEST_APPROVALS_ACTUAL}" | grep -q "${PYTEST_APPROVALS_EXPECTED}"; then
   echo "VERSION CONFIRMED as ${PYTEST_APPROVALS_EXPECTED}"
